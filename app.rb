f5f1e83105b38a7602d5a87c261c045a3eaee32f
imaizumimr <tesioa@gmail.com>
2020-02-06 23:40:36 +0900

    first commit

---
+++ app.rb
@@ -0,0 +1,99 @@
+# frozen_string_literal: true
+
+require "sinatra"
+require "sinatra/base"
+require "sinatra/reloader"
+
+class MemoApp < Sinatra::Base
+  configure :development do # なにこれ？
+    register Sinatra::Reloader
+    enable :method_override
+  end
+
+  helpers do
+    def get_memo_titles
+      titles = []
+      Dir.glob("texts/*").sort.each do |path|
+        File.open("#{path}", "r") { |f| titles << f.gets }
+      end
+      titles
+    end
+
+    def get_memo_paths
+      paths = []
+      Dir.glob("texts/*").sort.each { |path| paths << path }
+      paths
+    end
+
+    def create_memo
+      File.open("texts/#{Time.now.to_i}.txt", "w") do |f|
+        f.write(h(params[:message]))
+      end
+    end
+
+    def write_memo(path)
+      File.open("#{path}", "w") do |f|
+        f.write(h(params[:message]))
+      end
+    end
+
+    def delete_memo(path)
+      File.delete("#{path}")
+    end
+
+    def valid_memo_check(memo)
+      if memo.nil?
+        halt 404, "404 - そのメモはありません"
+      end
+    end
+
+    def set_path
+      paths = get_memo_paths
+      paths[params[:num].to_i]
+    end
+
+    def h(text)
+      Rack::Utils.escape_html(text)
+    end
+  end
+
+  get "/" do
+    @titles = get_memo_titles
+    erb :index
+  end
+
+  post "/" do
+    create_memo
+    redirect "/"
+  end
+
+  get "/new" do
+    erb :new
+  end
+
+  get "/:num" do
+    @path = set_path
+    valid_memo_check(@path)
+    erb :show
+  end
+
+  get "/:num/edit" do
+    @path = set_path
+    valid_memo_check(@path)
+    erb :edit
+  end
+
+  patch "/:num" do
+    @path = set_path
+    valid_memo_check(@path)
+    write_memo(@path)
+    redirect "/"
+  end
+
+  delete "/:num" do
+    @path = set_path
+    valid_memo_check(@path)
+    delete_memo(@path)
+    redirect "/"
+  end
+end
